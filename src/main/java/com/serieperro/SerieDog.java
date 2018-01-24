dcb83fd09039d983c2c075b68bc761747551c130
bianey <tanikdea@gmail.com>
2018-01-24 10:43:32 +0100

    Segunda intento de serialización + deserialización

--- src/main/java/com/serieperro/SerieDog.java
+++ src/main/java/com/serieperro/SerieDog.java
@@ -1,6 +1,5 @@
 package com.serieperro;
 
-import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
@@ -11,25 +10,34 @@ public class SerieDog {
     public static void main(String[] args) {
         ObjectMapper mapper = new ObjectMapper();
 
-        Dog one=new Dog(5,"Poodle","blanco");
-        Dog two=new Dog(10,"Bulldog","marrón");
-        Dog three=new Dog(15,"Dalmata","blanco y negro");
+        Dog[] pets=new Dog[3];
+
+        pets[0]=new Dog(5,"Poodle","blanco");
+        pets[1]=new Dog(10,"Bulldog","marrón");
+        pets[2]=new Dog(15,"Dalmata","blanco y negro");
+        File fileConValoresJson=new File("test.txt");
 
         try{
-            mapper.writeValue(new File("test.txt"), one);
-
-
-
-            //Convert object to JSON string
-            String jsonInString = mapper.writeValueAsString(one);
-            System.out.println(jsonInString);
-
-            //Convert object to JSON string and pretty print
-            //jsonInString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(one);
-            //System.out.println(jsonInString);
-
-        }catch (JsonGenerationException e) {
+            for (int i=0;i<3;i++){
+                mapper.writeValue(fileConValoresJson, pets[i]);
+                //Convert object to JSON string
+                String jsonInString = mapper.writeValueAsString(pets[i]);
+                System.out.println(jsonInString);
+                //Convert object to JSON string and pretty print
+                //jsonInString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(pets[i]);
+                //System.out.println(jsonInString);
+                //mapper.writeValue(fileConValoresJson, jsonInString);
+            }
+        } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        try {
+            // Deserialize JSON file into Java object.
+            Dog newDog = mapper.readValue(fileConValoresJson, Dog.class);
+            System.out.println("newDog.getEdad= " + newDog.getEdad());
+            System.out.println("newDog.getRaza= " + newDog.getRaza());
+            System.out.println("newDog.getColor= " + newDog.getColor());
         } catch (IOException e) {
             e.printStackTrace();
         }
